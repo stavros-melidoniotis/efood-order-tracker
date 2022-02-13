fea1cdf3c9d99af472725873769a301e6831463f
stavros-melidoniotis <melidon.stavros@gmail.com>
2022-02-13 22:22:20 +0200

    Get DOM elements by text content

--- order-tracking/foreground.js
+++ order-tracking/foreground.js
@@ -1,41 +1,44 @@
 const injectEfoodTracker = () => {
-    let waitingTime = document.querySelector("#thankyou-page > div > div > div > div > div > div > div > div > div > div:nth-child(2)").textContent
+    let waitingTime = getElementByXpath("//div[contains(text(),'Η παραγγελία σου θα παραδοθεί σε')]").textContent
     const waitingTimeParts = waitingTime.split(' ')
 
     waitingTime = waitingTimeParts[waitingTimeParts.length - 1]
 
-    const storeLogoUrl = document.querySelector("#thankyou-page > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(3) > div:nth-child(1) > img").getAttribute('src')
-    const storeName = document.querySelector("#thankyou-page > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(3) > div:nth-child(1) > div > div").textContent
-    const orderTotal = document.querySelector("#thankyou-page > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(3) > div:nth-child(3) > div > div").textContent
-    const storeTelephone = document.querySelector("#thankyou-page > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a > span").textContent
-    const orderItems = document.querySelectorAll("#thankyou-page > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(4) > div:first-of-type > div")
+    const storeName = getElementByXpath("//h4[contains(text(),'Κατάστημα')]").nextSibling.textContent
+    const storeLogoUrl = getElementByXpath("//h4[contains(text(),'Κατάστημα')]").parentElement.previousSibling.getAttribute('src')
+    const orderTotal = getElementByXpath("//h4[contains(text(),'Σύνολο πληρωμής')]").nextSibling.textContent
+    const storeTelephone = getElementByXpath("//div[contains(text(),'Επικοινωνία με το κατάστημα')]").nextSibling.nextSibling.lastChild.textContent
+    const orderItems = getElementByXpath("//h4[contains(text(),'Καλάθι')]").nextSibling.children
 
     const orderItemsArray = []
 
-    orderItems.forEach(orderItem => {
-        const quantity = orderItem.querySelector('div:first-of-type').textContent
-        const item = orderItem.querySelector('div:nth-child(2) > h3').textContent
-        const itemDetails = orderItem.querySelector('div:nth-child(2) > p')
-        const price = orderItem.querySelector('div:last-of-type').textContent
+    for (let orderItem of orderItems) {
+        const orderItemWrapper = orderItem.firstChild
+    
+        const quantity = orderItemWrapper.children[0].textContent
+        const item = orderItemWrapper.children[1]
+        const itemName = item.children[0].textContent
+        const itemDetails = (item.children.length > 1) ? item.children[1].textContent : ''
+        const price = orderItemWrapper.children[2].textContent
 
         orderItemsArray.push({
             quantity: quantity,
-            item: item,
-            itemDetails: (itemDetails) ? itemDetails.textContent : '',
+            item: itemName,
+            itemDetails: itemDetails,
             price: price
         })
-    })
+    }
 
     // Remove minutes symbol from time
     waitingTime = waitingTime.replace("'", "")
-    
+
     const currentDate = new Date().getTime()
     const estimatedArrivalTime = new Date(currentDate + waitingTime * 60000).getTime()
-    
+
     const urlString = window.location.href
     const url = new URL(urlString)
     const orderID = url.searchParams.get("order_id")
-    
+
     chrome.runtime.sendMessage({
         message: 'save_order_data',
         payload: {
@@ -52,11 +55,11 @@ const injectEfoodTracker = () => {
     }, response => {
         if (response.message === 'success') {
             console.log('New order data saved!');
-    
+
             let popup = document.createElement('div');
             popup.id = 'popup';
             popup.innerHTML = '<p> <span>Επιτυχής </span> προσθήκη παραγγελίας! </p>';
-    
+
             document.body.append(popup);
         }
     })
@@ -64,8 +67,14 @@ const injectEfoodTracker = () => {
 
 const ONE_SECOND = 1000
 
+const getElementByXpath = (path) => {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+}
+
 const checkIfElementPresent = setInterval(() => {
-    if (document.querySelector("#thankyou-page > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(3)")) {
+    let arrivalTimeDiv = getElementByXpath("//div[contains(text(),'Η παραγγελία σου θα παραδοθεί σε')]")
+
+    if (arrivalTimeDiv) {
         clearInterval(checkIfElementPresent)
         injectEfoodTracker()
     }
